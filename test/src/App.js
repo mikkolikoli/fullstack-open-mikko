69288cc0428c6bd21a89d7d2db13a4e145357a52
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2022-01-28 14:03:03 +0200

    Osa 2-1 test

--- test/src/App.js
+++ test/src/App.js
@@ -1,62 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Note from './components/Note.js'
 
-
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-}
-
-const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
-  const [allClicks, setAll] = useState([])
-
-  const handleLeftClick = () => {
-    const newClicks = {
-      left: clicks.left + 1,
-      right: clicks.right
-    }
-    setAll(allClicks.concat('L'))
-    setClicks(newClicks)
-  }
-
-  const handleRightClick = () => {
-    const newClicks = {
-      left: clicks.left,
-      right: clicks.right + 1
-    }
-    setAll(allClicks.concat('R'))
-    setClicks(newClicks)
-  }
+const App = ({ notes }) => {
 
   return (
     <div>
-      <div>
-        {clicks.left}
-        <Button handleClick={handleLeftClick} text='left' />
-        <Button handleClick={handleRightClick} text='right' />
-        {clicks.right}
-        <History allClicks={allClicks} />
-      </div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
