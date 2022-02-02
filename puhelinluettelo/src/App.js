8bfd532d9b4f88d7710aa7b30f20dd43bdfb2636
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2022-02-02 13:25:44 +0200

    json serveri puhelinluetteloon

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,16 +1,21 @@
-import React, {useState} from "react";
+import React, {useState, useEffect } from "react";
+import axios from 'axios';
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-  const [shownPersons, setShownPersons] = useState(persons)
+  const [persons, setPersons] = useState([])
+  const [shownPersons, setShownPersons] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setShownPersons(response.data)
+      })
+  }, [])
 
   return (
     <div>
